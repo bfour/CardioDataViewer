35f1d825bcba3a7cd1ffc5885a3f7d6b4622be97
Florian Pollak <fpdevelop@gmail.com>
2014-11-08 20:43:36 +0100

    initial commit

---
+++ CardioDataViewer/gen/at/ac/tuwien/e0826357/sinustest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package at.ac.tuwien.e0826357.sinustest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CardioDataViewer/src/at/ac/tuwien/e0826357/sinustest/ECGPlotter.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.e0826357.sinustest;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import android.graphics.Color;
+
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.LineAndPointRenderer;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+
+public class ECGPlotter {
+
+
+
+}

6a9629956de3661efe20ccd262c3ce428fbe8763
Florian Pollak <fpdevelop@gmail.com>
2014-11-16 22:01:18 +0100

    enabled automatic hiding of UI-elements (immersive mode)

--- CardioDataViewer/src/at/ac/tuwien/e0826357/cardioDataViewer/MainActivity.java
+++ CardioDataViewer/src/at/ac/tuwien/e0826357/cardioDataViewer/MainActivity.java
@@ -3,13 +3,15 @@ package at.ac.tuwien.e0826357.cardioDataViewer;
 import java.util.Observable;
 import java.util.Observer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.LinearLayout;
-import at.ac.tuwien.e0826357.cardioDataViewer.domain.ECTimeSeriesSegment;
 import at.ac.tuwien.e0826357.cardioDataViewer.service.CardiovascularDataService;
 import at.ac.tuwien.e0826357.cardioDataViewer.service.ServiceManager;
 import at.ac.tuwien.e0826357.cardioDataViewer.util.SystemUiHider;
@@ -31,6 +33,16 @@ import com.jjoe64.graphview.LineGraphView;
  */
 public class MainActivity extends Activity {
 
+	private static final boolean AUTO_HIDE = true;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = false;
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+	private SystemUiHider mSystemUiHider;
+
 	private CardiovascularDataService dataService;
 	private GraphView graph;
 
@@ -50,9 +62,9 @@ public class MainActivity extends Activity {
 			}
 		}
 
-		public void update(Observable obs, ECTimeSeriesSegment segment) {
+//		public void update(Observable obs, ECTimeSeriesSegment segment) {
 			// TODO
-		}
+//		}
 
 		public void update(Observable obs, GraphViewDataInterface data) {
 			series.appendData(data, false, 1861);
@@ -76,13 +88,78 @@ public class MainActivity extends Activity {
 		graph.setScalable(true);
 		graph.setShowLegend(true);
 		graph.setLegendAlign(LegendAlign.BOTTOM);
-		
+
 		graph.setManualMaxY(true);
 		graph.setManualMinY(true);
 		graph.setManualYMaxBound(1.0);
 		graph.setManualYMinBound(-1.0);
 
 		graphLayout.addView(graph);
+
+		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+
+		// Set up an instance of SystemUiHider to control the system UI for
+		// this activity.
+		mSystemUiHider = SystemUiHider.getInstance(this, graphLayout,
+				HIDER_FLAGS);
+		mSystemUiHider.setup();
+		mSystemUiHider
+				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+					// Cached values.
+					int mControlsHeight;
+					int mShortAnimTime;
+
+					@Override
+					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+					public void onVisibilityChange(boolean visible) {
+						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+							// If the ViewPropertyAnimator API is available
+							// (Honeycomb MR2 and later), use it to animate the
+							// in-layout UI controls at the bottom of the
+							// screen.
+							if (mControlsHeight == 0) {
+								mControlsHeight = controlsView.getHeight();
+							}
+							if (mShortAnimTime == 0) {
+								mShortAnimTime = getResources().getInteger(
+										android.R.integer.config_shortAnimTime);
+							}
+							controlsView
+									.animate()
+									.translationY(visible ? 0 : mControlsHeight)
+									.setDuration(mShortAnimTime);
+						} else {
+							// If the ViewPropertyAnimator APIs aren't
+							// available, simply show or hide the in-layout UI
+							// controls.
+							controlsView.setVisibility(visible ? View.VISIBLE
+									: View.GONE);
+						}
+
+						if (visible && AUTO_HIDE) {
+							// Schedule a hide().
+							delayedHide(AUTO_HIDE_DELAY_MILLIS);
+						}
+					}
+				});
+
+		// Set up the user interaction to manually show or hide the system UI.
+		graphLayout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (TOGGLE_ON_CLICK) {
+					mSystemUiHider.toggle();
+				} else {
+					mSystemUiHider.show();
+				}
+			}
+		});
+
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		findViewById(R.id.dummy_button).setOnTouchListener(
+				mDelayHideTouchListener);
 
 		// logic
 		GraphViewSeries channelOneSeries = new GraphViewSeries(
@@ -110,6 +187,48 @@ public class MainActivity extends Activity {
 		// TODO
 		super.onPause();
 		// dataService.stop();
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		delayedHide(100);
+	}
+
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mSystemUiHider.hide();
+		}
+	};
+
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
 
 }

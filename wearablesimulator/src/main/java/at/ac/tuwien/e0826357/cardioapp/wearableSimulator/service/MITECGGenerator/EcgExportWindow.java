3a4e0054f178add5744ba3c3ecb333569116245a
Florian Pollak <fpdevelop@gmail.com>
2016-11-14 04:27:51 +0100

    generated copyright headers for project

--- wearablesimulator/src/main/java/at/ac/tuwien/e0826357/cardioapp/wearableSimulator/service/MITECGGenerator/EcgExportWindow.java
+++ wearablesimulator/src/main/java/at/ac/tuwien/e0826357/cardioapp/wearableSimulator/service/MITECGGenerator/EcgExportWindow.java
@@ -1,3 +1,29 @@
+/*
+ * Java ECG Generator
+ * © Java Code Copyright(C) 2004 Mauricio Villarroel (m.villarroel@acm.org)
+ *
+ * Java ECG Generator was developed for ECGSYN:
+ *
+ * © ECGSYN Copyright by Patrick E. McSharry and Gari D. Clifford.
+ * For the Mathematical Model, see:
+ *       IEEE Transactions On Biomedical Engineering, 50(3), 289-294, March 2003
+ *
+ * Contact:
+ *      - Patrck McSharry (patrick@mcsharry.net)
+ *      - Gari Clifford (gari@mit.edu)
+ *
+ *
+ *
+ * Java ECG Generator and all its components are free software. You can redistribute
+ * them or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version. (See the GNU General Public License
+ * for more details.)
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 package at.ac.tuwien.e0826357.cardioapp.wearableSimulator.service.MITECGGenerator;
 
 /*
